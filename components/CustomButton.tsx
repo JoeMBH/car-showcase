cafbca3e6b5d8019cdc3deb2e93e84160b6ce575
JoeMBH <joembhu@gmail.com>
2023-06-19 11:55:10 +0300

    global styling

---
+++ components/CustomButton.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Image from "next/image";
+
+const CustomButton = () => {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn`}
+      onClick={() => {}}
+    ></button>
+  );
+};
+export default CustomButton;
---
+++ components/index.ts
@@ -0,0 +1,6 @@
+// This file will allow us to import components in an easier way, instead of having big imports atop every file.
+
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+
+export { Hero, CustomButton };
