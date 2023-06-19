b5bbb61bf75a66bf285552d377c54ef404c66003
JoeMBH <joembhu@gmail.com>
2023-06-19 12:26:05 +0300

    public assets added

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -2,14 +2,22 @@
 
 import Image from "next/image";
 
-const CustomButton = () => {
+import { CustomButtonProps } from "@/types";
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
   return (
     <button
       disabled={false}
       type={"button"}
-      className={`custom-btn`}
-      onClick={() => {}}
-    ></button>
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
   );
 };
 export default CustomButton;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import Image from "next/image";
+
 import CustomButton from "./CustomButton";
 
 const Hero = () => {
+  const handleScroll = () => {};
+
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -14,7 +19,21 @@ const Hero = () => {
           process.
         </p>
 
-        <CustomButton />
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="
+            bg-primary-blue 
+            text-white 
+            rounded-full 
+            mt-10
+          "
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image></Image>
+        </div>
       </div>
     </div>
   );
